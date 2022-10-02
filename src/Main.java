78ab699c1acc542a9d080f9843d74532f35c798b
Sahil <leviack629@gmail.com>
2022-10-02 22:46:25 +0530

    switch example

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,18 @@ public class Main {
         Scanner input = new Scanner(System.in);
         String fruit = input.next();
 
-
+        switch (fruit) {
+            case "Mango":
+                System.out.println("king of fruits!");
+                break;
+            case "Apple":
+                System.out.println("Sweet red fruit.");
+                break;
+            case "Orange":
+                System.out.println("A citrus fruit.");
+                break;
+            default:
+                System.out.println("Enter a valid fruit name");
+        }
     }
 }
